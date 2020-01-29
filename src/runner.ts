6dc5ea98f9a0c829893ee6a327b6336c01bce87b
nzws <git@nzws.me>
2020-01-29 14:41:44 +0900

    wip

--- src/runner.ts
+++ src/runner.ts
@@ -1,47 +1,56 @@
 type Command = {
   function: Function;
+  argsName?: Array<string>;
+  flags?: { [key: string]: string | Array<string> };
 };
-type CommandName = string | number;
-type Commands = { [key in CommandName]: Command };
+type Commands = { [key: string]: Command };
 
 type Config = {
   name: string;
   version: string;
   binName: string;
-  command: Command;
+  command?: Command;
   commands?: Commands;
-  args?: Array<CommandName>;
-};
-
-const functionSelector = (commands: Commands, arg: CommandName): Command => {
-  const hasHyphen: number = Object.keys(commands).findIndex(
-    key => key.slice(0, 1) === '-'
-  );
-  if (hasHyphen !== -1) {
-    throw new Error(
-      `[CLI ERROR] The beginning of the command name can't use a hyphen.: ${hasHyphen}`
-    );
-  }
-
-  const command: Command | undefined = commands[arg];
-  if (!command) {
-    throw new Error(`This command is not found.`);
-  }
-  return command;
+  default?: string;
+  args?: Array<string>;
 };
 
 const run = (config: Config): void => {
-  const args: Array<CommandName> =
+  const argv: Array<string> =
     config.args || process.argv.filter((v, index) => index > 1);
+  const flag: number = argv.findIndex(v => String(v).slice(0, 1) !== '-');
 
-  const flag: number = args.findIndex(v => String(v).slice(0, 1) !== '-');
-  if (flag !== -1) {
-    args.splice(flag, 1);
+  let command: Command | undefined;
+  if (flag !== -1 && config.commands) {
+    command = config.commands[argv[flag]];
+    if (!command) {
+      throw new Error(`This command is not found.`);
+    }
+    argv.splice(flag, 1);
+  } else {
+    // single or default
+    if (config.default && config.commands) {
+      command = config.commands[config.default];
+    } else if (config.command) {
+      command = config.command;
+    } else {
+      throw new Error(`Unknown command`);
+    }
   }
-  const command = config.commands
-    ? functionSelector(config.commands, args[flag])
-    : config.command;
-  command.function();
+
+  const args: Array<any> = argv.filter(v => v.slice(0, 1) !== '-');
+  const flags: { [key: string]: string | true } = {};
+  args
+    .filter(v => v.slice(0, 1) === '-')
+    .forEach(v => {
+      const data = v
+        .replace('--', '')
+        .replace('-', '')
+        .split('=');
+      flags[data[0]] = data[1] || true;
+    });
+
+  command.function({ args, flags });
 };
 
 export default run;
