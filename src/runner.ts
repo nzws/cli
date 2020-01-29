c354ddb58ad7fa2215091e2e1ab5d3a9428687f4
nzws <git@nzws.me>
2020-01-29 13:11:00 +0900

    wip

--- src/runner.ts
+++ src/runner.ts
@@ -1,30 +1,19 @@
 type Command = {
   function: Function;
 };
-
-type Commands =
-  | Command
-  | {
-      [key: string]: Command;
-    };
+type CommandName = string | number;
+type Commands = { [key in CommandName]: Command };
 
 type Config = {
   name: string;
   version: string;
   binName: string;
-  command: Commands;
-  args?: Array<string | number>;
-  default?: string | number;
+  command: Command;
+  commands?: Commands;
+  args?: Array<CommandName>;
 };
 
-const functionSelector = (
-  commands: Commands,
-  arg: string | undefined
-): Command => {
-  if (commands.function) {
-    return commands;
-  }
-
+const functionSelector = (commands: Commands, arg: CommandName): Command => {
   const hasHyphen: number = Object.keys(commands).findIndex(
     key => key.slice(0, 1) === '-'
   );
@@ -42,14 +31,16 @@ const functionSelector = (
 };
 
 const run = (config: Config): void => {
-  const args: Array<string | number> =
+  const args: Array<CommandName> =
     config.args || process.argv.filter((v, index) => index > 1);
 
-  const flag: number = args.findIndex(v => v.slice(0, 1) !== '-');
+  const flag: number = args.findIndex(v => String(v).slice(0, 1) !== '-');
   if (flag !== -1) {
     args.splice(flag, 1);
   }
-  const command = functionSelector(config.command, args[flag]);
+  const command = config.commands
+    ? functionSelector(config.commands, args[flag])
+    : config.command;
   command.function();
 };
 
